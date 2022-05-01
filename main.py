38cea1239f9c137de84912209ab85298f08c72bf
kerbs <73579653+kerthegreat@users.noreply.github.com>
2022-05-01 18:33:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,53 @@
 #Made with luv by k6r <3
-res = None
-while res not in {"addition", "subtraction", "multiplication", "division"}:
-    print("Choose Addition, subtraction, multiplication, division only.")
-    res = input("c h o o s e  y o u r  f a t e. ")
-    if res == "addition":
+class calc():
+
+    def add():
         add1 = int(input("input first number: "))
         add2 = int(input("what do you want to add? "))
         addData = add1 + add2
         print("the answer is", addData)
-    elif res == "subtraction":
+
+    def sub():
         sub1 = int(input("input first number: "))
         sub2 = int(input("what do you want to subtract? "))
         subData  = sub1 - sub2
         print("the answer is", subData)
-    elif res == "multiplication":
+
+    def mul():
         mul1 = int(input("input first number: "))
         mul2 = int(input("what do you want to multiply? "))
         mulData = mul1 * mul2
         print("the answer is", mulData)
-    elif res == "division":
+
+    def div():
         div1 = int(input("input first number: "))
         div2 = int(input("what do you want to divide? "))
         divData = div1 / div2
         print("the answer is", divData)
 
-input("Press enter to close program")
+asd = None
+while asd not in {"+", "-", "*", "/"}:
+    print("Choose Addition (+), subtraction (-), multiplication (*), division (/) only.")
+    asd = input("Please choose: ")
+        
+    if asd == ("+"):
+        calc.add()
+    elif asd == ("-"):
+        calc.sub()
+    elif asd == ("*"):
+        calc.mul()
+    elif asd == ("/"):
+        calc.div
+    else:
+        print("Please choose Addition (+), subtraction (-), multiplication (*), division (/) only.")
+        asd
+
+
+
+
+        
+
+
+
+
+
